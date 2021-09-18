d01f0d3f16ed7406a51d16507c6b85ed0b67f6d8
Venancio Avila <venanciodesouza@icloud.com>
2021-09-18 12:56:17 -0300

    add index

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const Gpio = require("pigpio").Gpio;
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
 const MICROSECDONDS_PER_CM = 1e6 / 34321;
 
+// HC-SR0$
 const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
 const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
+
+// LED
 
 trigger.digitalWrite(0); // Make sure trigger is low
 
@@ -27,4 +30,4 @@ watchHCSR04();
 // Trigger a distance measurement once per second
 setInterval(() => {
   trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 5000);
+}, 1000);
