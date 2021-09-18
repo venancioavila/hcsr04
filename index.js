9aa6b27adae695d773489b353fcbc840ed883d5d
Venancio Avila <venanciodesouza@icloud.com>
2021-09-18 13:03:34 -0300

    add index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
 const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
 
 // LED
-const led = new Gpio(4, { mode: Gpio.OUTPUT });
+const led = new Gpio(4, { mode: Gpio.INPUT });
 
 led.digitalWrite(0);
 trigger.digitalWrite(0); // Make sure trigger is low
