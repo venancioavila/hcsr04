c6a615ee504abe2275bc57e3e2457f5482a5e14f
Venancio Avila <venanciodesouza@icloud.com>
2021-09-18 14:02:01 -0300

    add index

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
 const led = new Gpio(20, { mode: Gpio.OUTPUT });
 const buzzer = new Gpio(16, { mode: Gpio.OUTPUT });
 
-while (true) {
-  setInterval(() => {
-    buzzer.digitalWrite(1); // Set trigger high for 10 microseconds
-  }, 1);
-  buzzer.digitalWrite(0);
-  console.log("bip");
-}
-
 led.digitalWrite(0);
 trigger.digitalWrite(0); // Make sure trigger is low
 
@@ -32,6 +24,8 @@ const watchHCSR04 = () => {
       const endTick = tick;
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       const distance = diff / 2 / MICROSECDONDS_PER_CM;
+
+      buzzer.pwmWrite(distance);
 
       if (distance < 70) {
         led.digitalWrite(1);
@@ -50,4 +44,4 @@ watchHCSR04();
 // Trigger a distance measurement once per second
 setInterval(() => {
   trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 500);
+}, 100);
