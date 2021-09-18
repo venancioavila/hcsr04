a5be92bd89bf9049ed362df3d2b58e571b94291e
Venancio Avila <venanciodesouza@icloud.com>
2021-09-18 12:32:31 -0300

    add index

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const Gpio = require("pigpio").Gpio;
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
 const MICROSECDONDS_PER_CM = 1e6 / 34321;
 
-const trigger = new Gpio(4, { mode: Gpio.OUTPUT });
-const echo = new Gpio(17, { mode: Gpio.INPUT, alert: true });
+const trigger = new Gpio(17, { mode: Gpio.OUTPUT });
+const echo = new Gpio(4, { mode: Gpio.INPUT, alert: true });
 
 trigger.digitalWrite(0); // Make sure trigger is low
 
