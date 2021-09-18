f66bd5eaee28e5d5681c8afe6799c41209ad6b95
Venancio Avila <venanciodesouza@icloud.com>
2021-09-18 13:07:54 -0300

    add index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
 // LED
 const led = new Gpio(4, { mode: Gpio.OUTPUT });
 
-led.pwmWrite(0);
+led.digitalWrite(0);
 trigger.digitalWrite(0); // Make sure trigger is low
 
 const watchHCSR04 = () => {
