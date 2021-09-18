b37a92bb3e7739ae4843c973ec32ef5bc2330560
Venancio Avila <venanciodesouza@icloud.com>
2021-09-18 13:34:05 -0300

    add index

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const watchHCSR04 = () => {
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       const distance = diff / 2 / MICROSECDONDS_PER_CM;
 
-      if (distance < 255) {
+      if (distance < 200) {
         led.pwmWrite(distance);
       }
 
