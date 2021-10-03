0d65ba805e0870ea10a8bbf5ed1ee280682fa671
Venancio Avila <venanciodesouza@icloud.com>
2021-10-03 16:30:11 -0300

    servo

--- servo.js
+++ servo.js
@@ -3,62 +3,32 @@ const board = new Board();
 
 const servo = () => {
   board.on("ready", () => {
-    const servo = new Servo(26);
+    // Create a new `servo` hardware instance.
+    const servo = new Servo(10);
 
-    // Servo alternate constructor with options
-    /*
-        var servo = new five.Servo({
-          id: "MyServo",     // User defined id
-          pin: 10,           // Which pin is it attached to?
-          type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
-          range: [0,180],    // Default: 0-180
-          fps: 100,          // Used to calculate rate of movement between positions
-          invert: false,     // Invert all specified positions
-          startAt: 90,       // Immediately move to a degree
-          center: true,      // overrides startAt if true and moves the servo to the center of the range
-        });
-        */
+    // Create a new `animation` instance.
+    const animation = new Animation(servo);
 
-    // Add servo to REPL (optional)
-    board.repl.inject({
-      servo,
+    // Enqueue an animation segment with options param
+    // See Animation example and docs for details
+    animation.enqueue({
+      cuePoints: [0, 0.25, 0.75, 1],
+      keyFrames: [
+        90,
+        { value: 180, easing: "inQuad" },
+        { value: 0, easing: "outQuad" },
+        90,
+      ],
+      duration: 2000,
     });
 
-    // Servo API
-
-    // min()
-    //
-    // set the servo to the minimum degrees
-    // defaults to 0
-    //
-    // eg. servo.min();
-
-    // max()
-    //
-    // set the servo to the maximum degrees
-    // defaults to 180
-    //
-    // eg. servo.max();
-
-    // center()
-    //
-    // centers the servo to 90°
-    //
-    // servo.center();
-
-    // to( deg )
-    //
-    // Moves the servo to position by degrees
-    //
-    // servo.to( 90 );
-
-    // step( deg )
-    //
-    // step all servos by deg
-    //
-    // eg. array.step( -20 );
-
-    servo.sweep();
+    // Inject the `servo` hardware into
+    // the Repl instance's context;
+    // allows direct command line access
+    board.repl.inject({
+      servo,
+      animation,
+    });
   });
 };
 
