db35a619a8e63cc6145f3157fa7bed792e91fc2f
Venancio Avila <venanciodesouza@icloud.com>
2021-10-03 17:48:15 -0300

    servo

--- servo.js
+++ servo.js
@@ -3,7 +3,7 @@ const Gpio = require("pigpio").Gpio;
 const start = () => {
   const motor = new Gpio(26, { mode: Gpio.OUTPUT });
 
-  motor.servoWrite(1000);
+  motor.servoWrite(180);
 };
 
 const stop = () => {
